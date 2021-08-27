8d8012065b901e9d98850412be01cf2695bd9195
Roton Robidash <roton2576@gmail.com>
2021-08-27 19:49:13 +0600

    mealDB

---
+++ mealdbassyn.js
@@ -0,0 +1,69 @@
+const btnClick = async() => {
+     let inputField = document.getElementById('input-field');
+     let inputFieldValue = inputField.value;
+     inputField.value = '';
+
+     const errorMessageDiv = document.getElementById('errorMessage');
+     if(inputFieldValue == '') {
+          const h1 = document.createElement('h1');
+          h1.innerText = 'Sorry! please input something';
+          h1.style.color = 'red';
+          errorMessageDiv.appendChild(h1);
+     } else {
+          const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputFieldValue}`;
+          const res = await fetch(url);
+          const data = await res.json();
+          searchResult(data.meals);
+     }
+}
+
+const searchResult = meals => {
+     const parentDivContainer = document.getElementById('parentDiv');
+     parentDivContainer.textContent = '';
+     const errorMessageDiv = document.getElementById('errorMessage');
+     if(meals.length == 0) {
+          const h1 = document.createElement('h1');
+          h1.innerText = 'Sorry! not found';
+          h1.style.color = 'blue';
+          errorMessageDiv.appendChild(h1);
+     } else {
+               meals.forEach(meal => {
+               const div = document.createElement('div');
+               div.classList.add('col');
+               div.innerHTML = `
+               <div onclick="loadMealDetails(${meal.idMeal})" class="card">
+                    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0, 250)}</p>
+                    </div>
+               </div>
+               `
+               parentDivContainer.appendChild(div);
+          });
+     }
+     
+}
+
+const loadMealDetails = async mealDetails => {
+     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealDetails}`;
+     const res = await fetch(url)
+     const data = await res.json();
+     displayMealDetails(data.meals[0])
+}
+
+const displayMealDetails = meal => {
+     const mealDetails = document.getElementById('meal-details');
+     mealDetails.textContent = '';
+     const div = document.createElement('div');
+     div.classList.add('card');
+     div.innerHTML = `
+     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+     <div class="card-body">
+       <h5 class="card-title">${meal.strMeal}</h5>
+       <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+       <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+     </div>
+     `
+     mealDetails.appendChild(div);
+}
